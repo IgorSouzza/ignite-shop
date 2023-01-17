779e9ba3ade8c7a7c954bc88b3a97393ddc36dfb
Igor Souza <igor.souza.webmaster@gmail.com>
2023-01-17 00:18:06 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
+import Image from 'next/image'
 import type { AppProps } from 'next/app'
 
+import * as S from '@/styles/pages/app'
+import { globalStyles } from '@/styles/global'
+import logoImg from '@/assets/logo.svg'
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <Image src={logoImg} alt="" />
+      </S.Header>
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
